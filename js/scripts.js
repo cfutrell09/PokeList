2b226f70d21178c067202b1eefc6f38bf33c69e9
cfutrell09 <cfutrell09@gmail.com>
2023-06-01 08:33:43 -0700

    created a new repository for PokeList
    New Repository for Pokelist

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+let pokemonList= [
+    {
+    name: "balbasaure", 
+    type: "grass", "poison"
+    height: 7
+    }
+
+    {
+        name: "charmander", 
+        type: "fire",
+        height: 6
+    }
+    
+    {
+        name: "pikachu", 
+        type: "lightning",
+        height: 4
+    }
+    
+    {
+        name: "squirtle", 
+        type: "water",
+        height: 5
+    }
+]
