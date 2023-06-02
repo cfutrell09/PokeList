bf5c94cdf91a0144767b2199972dcbcaa717203a
cfutrell09 <cfutrell09@gmail.com>
2023-06-02 12:23:01 -0700

    For Loop
    Added For Loop to render the name and the height of the pokemon, also with a message whether its a BIG ON, AVERAGE, or a TINY ONE.

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,14 @@ let pokemonList= [
         height: 5
     }
 ]
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height >=7){
+    document.write(pokemonList[i].name + pokemonList[i].height + " , He's a BIG ONE!!");
+}else if (pokemonList[i].height >4 && pokemonList[i].height <=6){
+    document.write(pokemonList[i].name + pokemonList[i].height + " , He's AVERAGE!!");
+}else {
+    document.write(pokemonList[i].name + pokemonList[i].height + ", He's a TINY ONE!!")
+}
+}
+
