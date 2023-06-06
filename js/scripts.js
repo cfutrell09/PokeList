1107d1236f529f696f1b42d73396b6e37fd7bc00
cfutrell09 <cfutrell09@gmail.com>
2023-06-05 17:40:14 -0700

    IIFE and forEach functions
    created and IIFE and forEach function to be able to call upon my Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,90 @@
-let pokemonList= [
-    {
-    name: "balbasaure", 
-    type: "grass", "poison"
-    height: 7
+//IIFE Function
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: "balbasaure", 
+            type: "grass", "poison"
+            height: 7
+        }
+
+        {
+            name: "charmander", 
+            type: "fire",
+            height: 6
+        }
+    
+        {
+            name: "pikachu", 
+            type: "lightning",
+            height: 4
+        }
+    
+        {
+            name: "squirtle", 
+            type: "water",
+            height: 5
+        }
+
+        {
+            name: "caterpie", 
+            type: "bug",
+            height: 3
+        }
+    
+        {
+            name: "clefairy", 
+            type: "fairy",
+            height: 6
+        }
+        
+        {
+            name: "jifflypuff", 
+            type: "fairy", "normal"
+            height: 5
+        }
+        
+        {
+            name: "diglett", 
+            type: "ground",
+            height: 2
+        }
+    ]
+    funtion add (pokemon) {
+        pokemonList.push(pokemon);
     }
 
-    {
-        name: "charmander", 
-        type: "fire",
-        height: 6
+    function getAll() {
+        return pokemonList;
     }
-    
-    {
-        name: "pikachu", 
-        type: "lightning",
-        height: 4
-    }
-    
-    {
-        name: "squirtle", 
-        type: "water",
-        height: 5
-    }
-]
+}) ();
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height >=7){
-    document.write(pokemonList[i].name + pokemonList[i].height + " , He's a BIG ONE!!");
-}else if (pokemonList[i].height >4 && pokemonList[i].height <=6){
-    document.write(pokemonList[i].name + pokemonList[i].height + " , He's AVERAGE!!");
-}else {
-    document.write(pokemonList[i].name + pokemonList[i].height + ", He's a TINY ONE!!")
-}
-}
+pokemonRepository.getAll.forEach(poke);
+
+
+// code that was here from previous exercises that I didnt know if I needed to delete LOL
+
+//function printArrayDetails(list){
+    //for (let i = 0; i < list.length; i++){
+        //document.write("<p>" + list[i].name + "</p>")
+       // console.log(list[i].name);
+    //}
+//}
+//for (let i = 0; i < pokemonList.length; i++) {
+    //if (pokemonList[i].height >=7){
+    //document.write(pokemonList[i].name + pokemonList[i].height + " , He's a BIG ONE!!");
+//}else if (pokemonList[i].height >4 && pokemonList[i].height <=6){
+    //document.write(pokemonList[i].name + pokemonList[i].height + " , He's AVERAGE!!");
+//}else {
+    //document.write(pokemonList[i].name + pokemonList[i].height + ", He's a TINY ONE!!")
+//}
+//}
+
+//printArrayDetails(pokemonList);
+
+
+//forEach function
+
+
+
+
 
